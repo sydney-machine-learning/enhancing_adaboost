23c4fb8346b75e6dd2f42436de4aeed99075eee6
colin823 <z5295493@ad.unsw.edu.au>
2021-10-09 17:17:35 +0800

    update

--- sklearn.Adaboost_2class.py
+++ sklearn.Adaboost_2class.py
@@ -1,35 +1,8 @@
 from sklearn.ensemble import AdaBoostClassifier
 import pandas as pd
-from sklearn.model_selection import train_test_split
-import random
+from data_processing import load_data
 
-
-filepath="data/abalone.data"
-df=pd.read_csv(filepath,header=None)
-df.columns = [ 'Sex', 'Length', 'Diameter','Height','Whole weight','Shucked weight','Viscera weight','Shell weight','Rings']
-df = df.replace('M',0)
-df = df.replace('F',1)
-df = df.replace('I',-1)
-
-df['Categories'] = pd.cut(df['Rings'], bins = [-1,9,1000], labels=[-1,1])
-
-n = 2000
-posN= int(n*0.8)
-negN= int(n*0.2)
-
-random.seed(1)
-pos_index = random.sample(list(df.query('Categories == 1').index),posN)
-
-neg_index = random.sample(list(df.query('Categories == -1').index),negN)
-samples_index = pos_index + neg_index
-data = df.iloc[samples_index]
-X = data.iloc[:, 0:7]
-y = data['Categories']
-# print(data)
-# print(y.value_counts())
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0,stratify=y)
+X_train, X_test, y_train, y_test = load_data('abalone_2class',test_size=0.2, random_state=0)
 
 clf = AdaBoostClassifier(n_estimators=30, random_state=0, algorithm="SAMME")
 
